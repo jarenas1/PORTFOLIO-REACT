6bf37187005aed2a0c85eed409c10b9120d27cfa
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-11-21 16:18:06 -0500

    create: technologies

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -18,40 +18,40 @@ const Technologies = () => {
         <h2 className='my-20 text-center text-4xl'>Tecnologías</h2>
         <div className='flex flex-wrap items-center justify-center gap-4'>
             <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <DiJava className='text-6xl text-cyan-400'/>
+                <DiJava className='text-6xl text-blue-400'/>
             </div>
             <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <SiSpringboot className='text-6xl text-cyan-400'/>
+                <SiSpringboot className='text-6xl text-green-600'/>
             </div>
             <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <FaLaravel className='text-6xl text-cyan-400'/>
+                <FaLaravel className='text-6xl text-red-400'/>
             </div>
             <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <RiReactjsLine className='text-6xl text-cyan-400'/>
+                <DiDocker className='text-6xl text-blue-700'/>
             </div>
             <div className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <SiGithubactions className='text-6xl text-cyan-400'/>
             </div>
             <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <DiDocker className='text-6xl text-cyan-400'/>
+                <SiMongodb className='text-6xl text-green-400'/>
             </div>
             <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <DiMysql className='text-6xl text-cyan-400'/>
+                <DiMysql className='text-6xl text-blue-500'/>
             </div>
             <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <SiPostgresql className='text-6xl text-cyan-400'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <SiMongodb className='text-6xl text-cyan-400'/>
+                <SiPostgresql className='text-6xl text-blue-700'/>
             </div>
             <div className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <RiReactjsLine className='text-6xl text-cyan-400'/>
             </div>
             <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <RiReactjsLine className='text-6xl text-cyan-400'/>
+                <DiJsBadge className='text-6xl text-yellow-400'/>
             </div>
             <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <RiReactjsLine className='text-6xl text-cyan-400'/>
+                <FaHtml5 className='text-6xl text-orange-500'/>
+            </div>
+            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+                <DiSass className='text-6xl text-pink-400'/>
             </div>
         </div>
     </div>
