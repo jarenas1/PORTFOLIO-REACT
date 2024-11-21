faffcab257e33bb7614a53a4f5b27f2b605fb72c
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-11-20 23:44:50 -0500

    create: navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import logo from "../assets/kevinRushLogo.png"
+import { FaLinkedin } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
+import { FaWhatsapp } from 'react-icons/fa' 
+
+const Navbar = () => {
+  return (
+    <>
+    {/* LOGO */}
+      <nav className='mb-20 flex items-center justify-between py-6'>
+        <div className='flex flex-shrink-0 items-center'>
+            <img src={logo} alt="jarenas1 logo" />
+        </div>
+        {/* CONTACT INFORMATION */}
+        <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+            <FaLinkedin />
+            <FaGithub/>
+            <FaInstagram/>
+            <FaWhatsapp/>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
