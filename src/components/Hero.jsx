05e36ed0fdf3e76960abaeaccfc0dfef8ca1816c
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-11-21 00:16:41 -0500

    create: right section of hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { HERO_CONTENT } from '../constants'
+import profilePic from "../assets/kevinRushProfile.png"
 
 const Hero = () => {
   return (
@@ -11,6 +12,11 @@ const Hero = () => {
                 <span className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 
                 bg-clip-text text-4xl tracking-tight text-transparent'>Desarrollador Full Stack</span>
                 <p className='my-2 max-w-xl py-6 font-light tracking-tighter'>{HERO_CONTENT}</p>
+            </div>
+        </div>
+        <div className='w-full lg:w-1/2 lg:p-8'>
+            <div className='flex justify-center'>
+                <img src={profilePic} alt="Juan Arenas" />
             </div>
         </div>
       </div>
